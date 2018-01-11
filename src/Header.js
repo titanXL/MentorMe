95d331ab8930a6bbf8b3be0970c4b4dd76f60eba
titanXL <emildavidkov88@gmail.com>
2018-01-11 22:28:29 +0200

    Added babel eslint configs

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
+import styles from './test.css'
 
-const Header = () => <h1>TESTING!!!</h1>
+const Header = () => <h1 className={styles.test}>TESTING!!!1</h1>
 
 export default Header
